0b532b66ac470d7b83115c17b7879505aec5b160
Jacob Landis <landisj@students.westerntc.edu>
2021-05-21 09:10:43 -0500

    Added privacy level and relationship tier properties to FullProfileRecord.ts.

--- SocialMedia/wwwroot/ts/Records/FullProfileRecord.ts
+++ SocialMedia/wwwroot/ts/Records/FullProfileRecord.ts
@@ -4,4 +4,11 @@
     lastName: string;
     profilePicture: ImageRecord;
     bio: string;
+    relationToUser: string;
+    relationshipTier: number;
+    profilePicturePrivacyLevel: number;
+    profileBioPrivacyLevel: number;
+    profileImagesPrivacyLevel: number;
+    profileFriendsPrivacyLevel: number;
+    profilePostsPrivacyLevel: number;
 }
